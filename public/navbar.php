0222f692db2276cd586d25c53d4d4a6498e01adc
Jesper <jesperminks7@gmail.com>
2021-09-20 13:32:30 +0200

    Update

---
+++ public/navbar.php
@@ -0,0 +1,13 @@
+<body>
+    <nav class="topnav">
+        <img src="content/dewalruslogo.png" alt="De Walrus Logo">
+
+        <a href="#">Home</a>
+        <a href="#">Menukaart</a>
+        <a href="#">Arrangementen</a>
+        <a href="#">Reserveren</a>
+        <a href="#">Contact</a>
+    </nav>
+</body>
+
+<link rel="stylesheet" href="css/navbar.css" />
