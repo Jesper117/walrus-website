f38f70ccb95da9e2d0d8c2c59bc00dc97df9f445
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2021-10-13 11:05:05 +0200

    ww wijzig

---
+++ test/index.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <form action="wachtwoordaanpassen.php" method="post">
+            <input type="password" name="ow" placeholder="Oud wachtword">
+            <input type="text" name="nww1" placeholder="Nieuw wachtwoord">
+        </form>
+    </body>
+</html>
