ea3794e992a13f2838eecb94938426e36d6f988a
Jesper <jesperminks7@gmail.com>
2021-10-13 11:03:38 +0200

    Update databaseconn.php

--- databaseconn.php
+++ databaseconn.php
@@ -1,13 +1,13 @@
-    <?php
-        
-        // $servername = "rdbms.strato.de";
-        // $username = "dbu1687307";
-        // $password = "fdekingmaSEITO21A";
-        // $dbname = "dbs4316475";
-        
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "testdatabase";
-        
-    ?>
+<?php
+    
+    // $servername = "rdbms.strato.de";
+    // $username = "dbu1687307";
+    // $password = "fdekingmaSEITO21A";
+    // $dbname = "dbs4316475";
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "testdatabase";
+    
+?>
