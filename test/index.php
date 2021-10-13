d5a572562bb54dc0773006411c9f187bb9789c40
MaartenVanNimwegen <89460431+MaartenVanNimwegen@users.noreply.github.com>
2021-10-13 11:07:03 +0200

    Update index.php

--- test/index.php
+++ test/index.php
@@ -3,6 +3,8 @@
         <form action="wachtwoordaanpassen.php" method="post">
             <input type="password" name="ow" placeholder="Oud wachtword">
             <input type="text" name="nww1" placeholder="Nieuw wachtwoord">
+            <input type="text" name="nww2" placeholder="Herhaal">
+            <input type="submit">
         </form>
     </body>
 </html>
